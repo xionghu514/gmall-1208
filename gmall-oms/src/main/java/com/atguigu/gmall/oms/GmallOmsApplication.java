ddf60c5338ff0125cedee72207e98471d55f532e
xionghu <1796235969@qq.com>
2022-12-09 21:07:12 +0800

    Add 网关搭建及配置完成

--- gmall-oms/src/main/java/com/atguigu/gmall/oms/GmallOmsApplication.java
+++ gmall-oms/src/main/java/com/atguigu/gmall/oms/GmallOmsApplication.java
@@ -1,9 +1,16 @@
 package com.atguigu.gmall.oms;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableFeignClients
+//@EnableDiscoveryClient // 将服务注册到服务中心, 默认会注册 可以省略
+@EnableSwagger2
+@MapperScan("com.atguigu.gmall.oms.mapper")
 public class GmallOmsApplication {
 
 	public static void main(String[] args) {
--- gmall-ums/src/main/java/com/atguigu/gmall/ums/GmallUmsApplication.java
+++ gmall-ums/src/main/java/com/atguigu/gmall/ums/GmallUmsApplication.java
@@ -1,9 +1,16 @@
 package com.atguigu.gmall.ums;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableFeignClients
+//@EnableDiscoveryClient // 将服务注册到服务中心, 默认会注册 可以省略
+@EnableSwagger2
+@MapperScan("com.atguigu.gmall.ums.mapper")
 public class GmallUmsApplication {
 
 	public static void main(String[] args) {
