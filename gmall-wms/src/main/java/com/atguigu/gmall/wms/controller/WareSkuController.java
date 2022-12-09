8282146ee375f569a16b98a6b9963b9087c89011
xionghu <1796235969@qq.com>
2022-12-09 22:54:04 +0800

    Add baseCrud 6.根据 skuId 查询 库存

--- gmall-wms/src/main/java/com/atguigu/gmall/wms/controller/WareSkuController.java
+++ gmall-wms/src/main/java/com/atguigu/gmall/wms/controller/WareSkuController.java
@@ -5,6 +5,7 @@ import com.atguigu.gmall.common.bean.PageResultVo;
 import com.atguigu.gmall.common.bean.ResponseVo;
 import com.atguigu.gmall.wms.entity.WareSkuEntity;
 import com.atguigu.gmall.wms.service.WareSkuService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +33,16 @@ public class WareSkuController {
     @Autowired
     private WareSkuService wareSkuService;
 
+    @GetMapping("/sku/{skuId}")
+    @ApiOperation("根据 skuId 查询 库存")
+    public ResponseVo<List<WareSkuEntity>> queryWareSkuEntities(@PathVariable("skuId") Long skuId) {
+        List<WareSkuEntity> wareSkuEntities = wareSkuService.list(
+                new LambdaQueryWrapper<WareSkuEntity>()
+                        .eq(WareSkuEntity::getSkuId, skuId)
+        );
+
+        return ResponseVo.ok(wareSkuEntities);
+    }
 
     /**
      * 列表
